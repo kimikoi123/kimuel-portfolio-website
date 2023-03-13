3c026317fec0e3d3f84cbdce142cfd03ff5537bf
Kimuel Arvin <83416645+kimikoi123@users.noreply.github.com>
2023-03-13 19:24:09 +0800

    updated socials

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
 
 export default function SocialMedia() {
   return (
     <div className='app__social'>
         <div>
-            <BsTwitter />
+            <a href='https://github.com/kimikoi123'><BsGithub /></a>
         </div>
         <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
+            <a href='https://www.linkedin.com/in/kimuel-anqui/'><BsLinkedin /></a>
         </div>
     </div>
   )
